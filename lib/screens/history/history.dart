fc4533c41209f5e93d7f6af8c259a8d8e38d3176
walelgn <walelgnlinux@gmail.com>
2022-04-01 18:56:15 +0300

    history and credit updated

--- lib/screens/history/history.dart
+++ lib/screens/history/history.dart
@@ -2,9 +2,10 @@ import 'package:driverapp/models/trip/trip.dart';
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatelessWidget {
-  final _textStyle = const TextStyle(fontSize: 15,color: Colors.deepOrange);
+  final _textStyle = const TextStyle(fontSize: 15, color: Colors.deepOrange);
 
   static const routeName = "/history";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,44 +33,55 @@ class HistoryPage extends StatelessWidget {
     const color = Colors.grey;
     const hoverColor = Colors.white70;
     return Card(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15.0))),
       elevation: 1,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          ListTile(
-            trailing: Text(
-              "\$40",
-              style: _textStyle,
-            ),
-            //leading: Icon(Icons.history, color: color.shade700),
-            title: Text(text, style: _textStyle),
-            subtitle: const Text(
-              "From: 4Kilo to: Bole",
-              style: TextStyle(fontSize: 15,color: Colors.black),
-            ),
-            hoverColor: hoverColor,
-            onLongPress: () {},
-            onTap: () {
-              showModalBottomSheet<void>(
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20.0),
-                        topRight: Radius.circular(20.0)
+      child: Container(
+        height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ListTile(
+              trailing: Text(
+                "\$40",
+                style: _textStyle,
+              ),
+              //leading: Icon(Icons.history, color: color.shade700),
+              title: Text(text, style: _textStyle),
+              subtitle: const Text(
+                "From: 4Kilo to: Bole",
+                style: TextStyle(fontSize: 15, color: Colors.black),
+              ),
+              hoverColor: hoverColor,
+              onLongPress: () {},
+              onTap: () {
+                showModalBottomSheet<void>(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20.0),
+                          topRight: Radius.circular(20.0)),
                     ),
-                  ),
-                  context: context,
-                  builder: (BuildContext context) {
-                    return _showBottomMessage(context,
-                        Trip(id:"f",date:text,time:"today",from:"4Kilo",to:"Bole",price:"\$40"));
-                  });
-            },
-          ),
-        ],
+                    context: context,
+                    builder: (BuildContext context) {
+                      return _showBottomMessage(
+                          context,
+                          Trip(
+                              id: "f",
+                              date: text,
+                              time: "today",
+                              from: "4Kilo",
+                              to: "Bole",
+                              price: "\$40"));
+                    });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _showBottomMessage(BuildContext context ,Trip trip) {
+  Widget _showBottomMessage(BuildContext context, Trip trip) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.3,
       child: Column(
@@ -85,7 +97,7 @@ class HistoryPage extends StatelessWidget {
             ),
             subtitle: Text(trip.date),
             trailing:
-            Text(trip.price, style: const TextStyle(color: Colors.red)),
+                Text(trip.price, style: const TextStyle(color: Colors.red)),
           ),
           Text(
             'from: ${trip.from} to: ${trip.to}',
